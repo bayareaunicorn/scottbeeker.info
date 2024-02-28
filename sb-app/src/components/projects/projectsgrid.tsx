e635766d0e411de05478baee536dcdb8651f157a
bayareaunicorn <scottbeeker415@gmail.com>
2024-02-28 04:44:17 -0800

    ts projects boiler

---
+++ sb-app/src/components/projects/projectsgrid.tsx
@@ -0,0 +1,25 @@
+import { useState, useCallback, useEffect, useRef, useMemo } from "react";
+
+
+interface ProjectGridProps {
+   title: string;
+   date: number;
+   description: string;
+   category: string;
+}
+
+
+
+
+const ProjectGrid: React.FC<ProjectGridProps> = ({ title, date, description, category }) => {
+    return (
+        <div className="projectcontainer">
+            <p>Title: {title}</p>
+            <p>Date: {date}</p>
+            <p>Description: {description}</p>
+            <p>Category: {category}</p>
+        </div>
+    )
+}
+
+export default ProjectGrid;
