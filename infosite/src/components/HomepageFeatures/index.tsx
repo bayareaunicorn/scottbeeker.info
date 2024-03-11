acf47164ba0a2531dd33fefe9fa0d3a1268b5528
bayareaunicorn <scottbeeker415@gmail.com>
2024-03-11 03:07:38 -0700

    homepagefeatures draft

--- infosite/src/components/HomepageFeatures/index.tsx
+++ infosite/src/components/HomepageFeatures/index.tsx
@@ -10,33 +10,28 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Powering the creator community',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Helping consumers connect with creators and brands in new innovative ways. 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Building the future',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+        Tearing down the garden walls of traditional social media while powering the fediverse with the ActivityPlex protocol.</>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Opensource',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+        Great platforms exist because of Opensource software, giving back to the community by Opensourcing most tooling and research.</>
     ),
   },
 ];
